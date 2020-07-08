2694af32f4e3de060d4da8a269ae37e580313e8b
firoz.anawar <firoz.anawar@timesinternet.in>
2020-07-08 22:49:29 +0530

    Leetcode RomanToInteger13 problem added

---
+++ leetcode/RomanToInteger13.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger13 {
+
+    public static void main(String[] args) {
+
+        String inputString = "IV";
+        System.out.println(romanToInt(inputString));
+    }
+
+    public static int romanToInt(String s) {
+
+        if (s == null || s.length() == 0) return 0;
+
+        Map<Character, Integer> hm = new HashMap<>();
+        hm.put('I', 1);
+        hm.put('V', 5);
+        hm.put('X', 10);
+        hm.put('L', 50);
+        hm.put('C', 100);
+        hm.put('D', 500);
+        hm.put('M', 1000);
+
+        int length = s.length();
+        int result = hm.get(s.charAt(length - 1));
+        System.out.println("Result: " + result);
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+
+            if (hm.get(s.charAt(i)) >= hm.get(s.charAt(i + 1))) {
+                result = result + hm.get(s.charAt(i));
+            } else {
+                result = result - hm.get(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
+}

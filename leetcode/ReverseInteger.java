8d50acdf9fb70b50c268d8ea9c57e5330c234e42
firoz.anawar <firoz.anawar@timesinternet.in>
2020-07-08 20:41:35 +0530

    Initial commit

---
+++ leetcode/ReverseInteger.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * Problem statement link : https://leetcode.com/problems/reverse-integer/
+ * Solution :
+ */
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+        int number  = 120;
+        System.out.println(reverse(number));
+    }
+
+    public static int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
---
+++ leetcode/TwoSum1.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.HashMap;
+
+/**
+ * Problem statement link : https://leetcode.com/problems/two-sum/
+ * Solution : https://www.callicoder.com/two-sum-problem/
+ */
+public class TwoSum1 {
+
+    public static void main(String[] args) {
+
+        int[] inputArray = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] outPut = twoSum(inputArray, target);
+        System.out.println(outPut[0]+ " "+outPut[1]);
+    }
+
+    // Time complexity: O(n)
+    public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (hm.containsKey(complement)) {
+                return new int[]{hm.get(complement), i};
+            } else {
+                hm.put(nums[i], i);
+            }
+        }
+
+        return new int[]{};
+    }
+
+    // Time complexity: O(n^2)
+    private static int[] findTwoSum_BruteForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+}
